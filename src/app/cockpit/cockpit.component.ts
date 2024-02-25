b0a5a7e7e16238668737d94b9cc2315dc0070d57
narongsak keawmanee <klogic@hotmail.co.th>
2024-02-25 18:39:53 +0700

    feat: passing as local reference template

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -7,27 +7,26 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class CockpitComponent {
   serverElements = [];
-  newServerName = '';
   newServerContent = '';
 
   @Output() serverCreated = new EventEmitter<{
     serverName: string;
     serverContent: string;
   }>();
-  @Output() blueprintCreated = new EventEmitter<{
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{
     serverName: string;
     serverContent: string;
   }>();
-  onAddServer() {
+  onAddServer(serverNameInput: HTMLInputElement) {
     this.serverCreated.emit({
-      serverName: this.newServerName,
+      serverName: serverNameInput.value,
       serverContent: this.newServerContent,
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(serverNameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
+      serverName: serverNameInput.value,
       serverContent: this.newServerContent,
     });
   }
